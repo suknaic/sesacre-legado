2ba9b4d139fbc51acc0820c59214e1e5405f53df
Maçao Saito <cmassao@gmail.com>
2018-06-04 11:10:02 -0500

    Corrigido a nomenclatura para adicionar Itinerario no relatório de viagem

--- pages/diarias/decreto_valor/index.php
+++ pages/diarias/decreto_valor/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/model/index.load.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/model/diarias/decreto_valor/index.load.php";
 ?>
 <html lang="pt-br">
     <head>
