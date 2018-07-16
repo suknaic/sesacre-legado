2a39b94ac17b48e6c49a88d32b50b578638732ba
Maçao Saito <cmassao@gmail.com>
2018-07-16 14:18:10 -0500

    Corrigido o caminho do arquivo que é carregado na listagem da vinculação das centrais e responsáveis dentro do módulo financeiro

--- model/financeiro/central_responsavel/index.load.php
+++ model/financeiro/central_responsavel/index.load.php
@@ -2,7 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/util/Session.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/rh/Contrato.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/central/FinCentralModel.class.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/fin/TipoAdministracao.class.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/class/administracao/tipo_administracao/TipoAdministracao.class.php";
 
 $session = new Session();
 
