182955b6b2037cb075f594029c680b262a3756de
Marcel Melo <marcel.melo@ac.gov.br>
2018-07-19 17:59:46 -0500

    aaditivo

--- assets/lib/sesacre/funcoes.js
+++ assets/lib/sesacre/funcoes.js
@@ -219,6 +219,17 @@ function Funcoes() {
         valor = parseFloat(valor.replace(',' , '.'));
         return valor;
     }
+    /**
+     * 
+     * @param string valor
+     * @returns {Number}
+     */
+    this.arrendondaValorParaQuatroCasas = function(valor){   
+        let a = Math.roundBy(5, valor);
+        a = Math.roundBy(4, a);
+        return a.toFixed(4);
+        //return (Math.round10(valor, -4)).toFixed(4);        
+    }
 
 
 }
@@ -249,4 +260,48 @@ function somenteNumeros(num) {
         campo.value = "";
     }
 }
+
+function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+            return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+            return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
+
+// Decimal round
+if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+            return decimalAdjust('round', value, exp);
+    };
+}
+// Decimal floor
+if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+            return decimalAdjust('floor', value, exp);
+    };
+}
+// Decimal ceil
+if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+            return decimalAdjust('ceil', value, exp);
+    };
+}
+
+Math.roundBy = function (decimalPlaces, n) {
+    var scale = Math.pow(10, decimalPlaces);
+    return Math.round(scale * n) / scale;
+};
+
 
