388393e29be016f8633cbe36d5ef8689ebeb77cb
Marcel Melo <marcel.melo@ac.gov.br>
2018-08-21 11:30:00 -0500

    aaa

--- class/financeiro/ordem/FinEntregaConfirmacaoModel.class.php
+++ class/financeiro/ordem/FinEntregaConfirmacaoModel.class.php
@@ -621,9 +621,7 @@ class FinEntregaConfirmacaoModel {
             foreach ($idEntregas as $linha) {
                 $arrayIdEntregas [] = $linha;
             }
-            echo "<pre>";
-            print_r($arrayIdEntregas);
-            echo "</pre>";
+            
             $idEntregas = implode(' , ', $arrayIdEntregas);
 
             $daoFinEntregaConfirmacao = new DaoFinEntregaConfirmacao();
