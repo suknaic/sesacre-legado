9ae541e8629a58e615ead97e435d44924acb0d98
Maçao Saito <cmassao@gmail.com>
2018-09-19 11:03:29 -0500

    ajuste numero da entrega

--- class/financeiro/gdof/FinDocumentoFiscal.class.php
+++ class/financeiro/gdof/FinDocumentoFiscal.class.php
@@ -1190,10 +1190,10 @@ class FinDocumentoFiscal {
                 foreach ($daoFinDocumentoFiscal->getMsgRetorno() as $key => $campos) {
                     $totalEntrega += $campos["vl_entrega_documento"];
                     
-                    $nr_entrega = ($campos['sit_entrega'] == 0) ? "0" : $campos["nr_entrega_confirmacao"] ;
+//                    $nr_entrega = ($campos['sit_entrega'] == 0) ? "0" : $campos["nr_entrega_confirmacao"] ;
                     
                     $tabela .= '<tr id= "ent' . $campos["id_entrega_confirmacao"] . '" ordem = "' . $campos["id_ordem"] . '" class = "trEntregas" data-situacao='.$campos['sit_entrega'].' idEntrega = "' . $campos["id_entrega_confirmacao"] . '">
-                                 <td class = "text-center">' . $nr_entrega . '</td>
+                                 <td class = "text-center">' . $campos["nr_entrega_confirmacao"] . '</td>
                                  <td class = "text-center">' . $campos["ordem"] . '</td>
                                  <td class = "text-center">' . $campos["dataaviso"] . '</td>
                                  <td class = "text-center">' . $campos["datalimite"] . '</td>
