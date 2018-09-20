af1005d609efbd075a7f9489352b35466a26df05
Maçao Saito <cmassao@gmail.com>
2018-09-20 13:43:36 -0500

    mudanças na edição e visualização do gdof

--- pages/financeiro/gdof/documentoFiscal/edit_documento/index.php
+++ pages/financeiro/gdof/documentoFiscal/edit_documento/index.php
@@ -128,7 +128,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/pages/financeiro/gdof/documentoFiscal
                                                                             <th class="text-center">Nº da Ordem</th>
                                                                             <th class="text-center">Tipo ordem</th>
                                                                             <th class="text-center">Valor Total</th>
-                                                                            <th class="text-center">Situacao</th>
+                                                                            <th class="text-center">Situação</th>
                                                                             <th class="text-center">Ação</th>
                                                                         </tr>
                                                                     </thead>
@@ -152,7 +152,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/pages/financeiro/gdof/documentoFiscal
                                             <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                                                 <div class="panel panel-default">
                                                     <div class="panel-heading" role="tab" id="headingTwo">
-                                                        <h4 class="panel-title">Dados da Entrega</h4>
+                                                        <h4 class="panel-title">Dados da Entrega ou Execução/Serviço</h4>
                                                     </div>
                                                     <div class="panel-body">
                                                         <div class="form-group">
