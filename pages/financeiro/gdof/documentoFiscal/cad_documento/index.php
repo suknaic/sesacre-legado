8d40c841aaa56c8c15216588fa49be35346d088c
Joellytton mendonça dos santos <joellytton.mendonca@ac.gov.br>
2018-09-21 16:40:04 -0500

    escondendo o botao de edicao do documento fiscal

--- pages/financeiro/gdof/documentoFiscal/cad_documento/index.php
+++ pages/financeiro/gdof/documentoFiscal/cad_documento/index.php
@@ -259,7 +259,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/pages/financeiro/gdof/documentoFiscal
                                         </div>
                                     </div>
                                 </div>
-                     
+
                                 <div class="form-group">
                                     <div  class="col-sm-12" style="margin-bottom: -4%;">
                                         <div class="panel-body ordem">
@@ -418,6 +418,11 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/pages/financeiro/gdof/documentoFiscal
                                         </div>
                                     </div>
                                 </div>
+
+                                <?php
+                                //Modal anotacoes
+                                require_once $_SERVER['DOCUMENT_ROOT'] . "/pages/financeiro/gdof/documentoFiscal/anotacao_documento/anotacao.html";
+                                ?>
 
                                 <div class="form-group">
                                     <div  class="col-sm-12">
