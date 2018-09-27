585db8e890538c3b602b9bfd1ec3910e9a57eff4
Joellytton mendonça dos santos <joellytton.mendonca@ac.gov.br>
2018-09-27 14:55:03 -0500

    atualiza

--- pages/contabil/pagamento/cad_pagamento/request.php
+++ pages/contabil/pagamento/cad_pagamento/request.php
@@ -4,13 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/class/util/Session.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/orcamento/empenho/FinEmpenhoModel.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/pedido/Pedido.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/compras/contrato/FinContratoModel.class.php";
-
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/ordem/FinOrdemModel.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/ordem/FinEntregaConfirmacaoModel.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/contabil/liquidacao/Liquidacao.class.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/class/contabil/liquidacao/LiquidacaoDoc.class.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/class/contabil/liquidacao/LiquidacaoHistorico.class.php";
-
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/financeiro/gdof/FinDocumentoFiscal.class.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/sistema/vincular_tramitacao/VincularTramitacao.class.php";
 
@@ -21,9 +17,9 @@ switch ($_REQUEST['acao']) {
     CASE 'retornaLiquidacao':
         try {
             $dados = filter_input(INPUT_GET, 'dados', FILTER_DEFAULT);
-            $finEmpenhoModel = new FinEmpenhoModel();
-            $finEmpenhoModel->setNrEmpenho($dados);
-            echo $finEmpenhoModel->trEmpenhoBuscaLiquidacao();
+            $liquidacao = new Liquidacao();
+            $liquidacao->setNrLiquidacao($dados);
+//            echo $finEmpenhoModel->trEmpenhoBuscaLiquidacao();
             return;
             break;
         } catch (Error $e) {
