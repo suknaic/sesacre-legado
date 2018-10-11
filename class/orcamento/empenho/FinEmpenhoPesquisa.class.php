ca026c86a0958620166b0562f65e6c8b867f6d70
Maçao Saito <cmassao@gmail.com>
2018-10-11 12:44:19 -0500

    Finalizado o filtro de consulta do empenho

--- class/orcamento/empenho/FinEmpenhoPesquisa.class.php
+++ class/orcamento/empenho/FinEmpenhoPesquisa.class.php
@@ -10,7 +10,17 @@ class FinEmpenhoPesquisa {
     private $nr_contrato = null;
     private $nr_pedido = null;
     private $tipo_gasto = null;
+    private $central = null;
     private $situacao = null;
+    
+    function getCentral() {
+        return $this->central;
+    }
+
+    function setCentral($central) {
+        $this->central = $central;
+        return $this;
+    }
     
     function getNrEmpenho() {
         return $this->nr_empenho;
@@ -101,7 +111,7 @@ class FinEmpenhoPesquisa {
             $pdo = $conexao->connect();
             $tabela = '';
             $daoFinEmpenho = new DaoFinEmpenho();
-            $daoFinEmpenho->retornaEmpenhos($pdo);
+            $daoFinEmpenho->retornaEmpenhos($pdo, $this->filtroSql());
             if ($daoFinEmpenho->sucesso()) {
                 foreach ($daoFinEmpenho->getMsgRetorno() as $linha) {
                     $tabela .= '<tr>'
@@ -109,19 +119,107 @@ class FinEmpenhoPesquisa {
                                 . '<td class="text-center">'.$linha['nr_pedido'].'</td>'
                                 . '<td class="text-center">'.$linha['cpf_cnpj'] . ' - '. $linha['nome_razao'].'</td>'
                                 . '<td class="text-center">'.$linha['nm_tipo_empenho'].'</td>'
-                                . '<td class="text-center">'.$linha['competencia'].'</td>'
                                 . '<td class="text-center">'.$linha['dt_empenho_safira'].'</td>'
+                                . '<td class="text-center">'.$linha['nm_tipo_gasto'].'</td>'
+                                . '<td class="text-center">'.$linha['central_demanda'].'</td>'
                                 . '<td class="text-center">'.$linha['vl_empenho'].'</td>'
                                 . '<td class="text-center">'.$linha['situacao'].'</td>'
-                                . '<td class="text-center"></td>'
+                                . '<td class="text-center">'
+                                    . '<button type="button" title="Ver Empenho" class="ver-empenho" value='.$linha['id_empenho'].'>'
+                                        . '<i class="fa fa-file-text-o text-info" aria-hidden="true"></i>'
+                                    . '</button>'
+                                . '</td>'
                             . '</tr>';
                 }
-            } else {
-                return Metodos::retornoAjax("Erro", "alert", $daoFinEmpenho->getMsgRetorno() );
-            }
+            } 
             return $tabela;
         } catch (Exception $exc) {
             return Metodos::retornoAjax("Erro", "alert", STR_ERROR );
+        }
+    }
+    
+    private function filtroSql(){
+
+        $array_filtro = array();
+        $and_ou_where = '';
+        
+        try {
+            
+            if (!empty($this->getNrEmpenho())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "emp.nr_empenho ilike :nr_empenho", 
+                    'bind' => ':nr_empenho',
+                    'valor' => '%'. $this->getNrEmpenho() .'%',
+                    'pdo_param' => PDO::PARAM_STR);
+            }
+            
+            if (!empty($this->getAnoExercicio())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "to_char(dt_empenho_safira,'YYYY') = :ano_exercicio",
+                    'bind' => ':ano_exercicio',
+                    'valor' => $this->getAnoExercicio(),
+                    'pdo_param' => PDO::PARAM_INT);
+            }
+            
+            if (!empty($this->getFornecedor())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "fornec.id_pessoa = :fornecedor",
+                    'bind' => ':fornecedor',
+                    'valor' => $this->getFornecedor(),
+                    'pdo_param' => PDO::PARAM_INT);
+            }
+            
+            if (!empty($this->getNrContrato())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "cnt.nr_contrato ilike :nr_contrato", 
+                    'bind' => ':nr_contrato',
+                    'valor' => '%'. $this->getNrContrato() .'%',
+                    'pdo_param' => PDO::PARAM_STR);
+            }
+            
+            if (!empty($this->getNrPedido())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "ped.nr_pedido ilike :nr_pedido", 
+                    'bind' => ':nr_pedido',
+                    'valor' => '%'. $this->getNrPedido() .'%',
+                    'pdo_param' => PDO::PARAM_STR);
+            }
+            
+            if (!empty($this->getTipoGasto())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "tpGasto.id_tipo_gasto = :tipo_gasto",
+                    'bind' => ':tipo_gasto',
+                    'valor' => $this->getTipoGasto(),
+                    'pdo_param' => PDO::PARAM_INT);
+            }
+            
+            if (!empty($this->getSituacao())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "emp.sit_empenho = :situacao",
+                    'bind' => ':situacao',
+                    'valor' => $this->getSituacao(),
+                    'pdo_param' => PDO::PARAM_STR);
+            }
+            
+            if (!empty($this->getCentral())) {
+                $and_ou_where = empty($array_filtro) ? " where " : " and ";
+                $array_filtro[] = array(
+                    'sql' => $and_ou_where . "central.id_lotacao = :central",
+                    'bind' => ':central',
+                    'valor' => $this->getCentral(),
+                    'pdo_param' => PDO::PARAM_INT);
+            }
+            
+            return $array_filtro;
+        } catch (Exception $exc) {
+            echo $exc->getMessage();
         }
     }
 
--- pages/contabil/empenho/pesquisa_empenho/index.js
+++ pages/contabil/empenho/pesquisa_empenho/index.js
@@ -6,19 +6,35 @@ $(document).ready(function () {
         width: '100%'
     });
     
-    lista();
+    $('body').on('click','.btn-pesquisar', function(){
+        var dados = {
+            'nr_empenho': $("#nr_empenho").val(),
+            'ano_exercicio': $("#ano_exercicio option:selected").val(),
+            'fornecedor': $("#fornecedor option:selected").val(),
+            'nr_pedido': $("#nr_pedido").val(),
+            'nr_contrato': $("#nr_contrato").val(),
+            'tipo_gasto': $("#tipo_gasto option:selected").val(),
+            'situacao': $("#situacao option:selected").val(),
+            'central': $("#central option:selected").val()
+        }
+        
+        lista(dados);
+    });
+    
+    
 });
 
-function lista() {
-    
+function lista(dados) {
     $.ajax({
         "url": "request.php",
         "dataType": 'html',
         "data": {
-            "acao": "retornaEmpenhos"
+            "acao": "retornaEmpenhos",
+            "dados": dados
         },
         "success": function (response) {
-            func.carregaTabelaPadrao('tabela', response, [4], true);
+//            console.log(response);
+            func.carregaTabelaPadrao('tabela', response, [9], true);
         }
     });
 }
