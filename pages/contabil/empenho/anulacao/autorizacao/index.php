f4cf096761378005bfa97c1ae027505c824f804e
Joellytton mendonça dos santos <joellytton.mendonca@ac.gov.br>
2018-10-22 14:17:06 -0500

    atualiza

--- pages/contabil/empenho/anulacao/autorizacao/index.php
+++ pages/contabil/empenho/anulacao/autorizacao/index.php
@@ -49,7 +49,7 @@ require_once "index.load.php";
                     <!--Page Title-->
                     <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
                     <div id="page-title">
-                        <h1 class="page-header text-overflow">Anulação do Empenho</h1>                       
+                        <h1 class="page-header text-overflow">Autorização da Anulação do Empenho</h1>                       
                     </div>
                     <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
                     <!--End page title-->
