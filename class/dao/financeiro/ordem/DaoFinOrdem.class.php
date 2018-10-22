cb4f600103364845bec6ded909591a98f4e38dda
Joellytton mendonça dos santos <joellytton.mendonca@ac.gov.br>
2018-10-22 12:02:06 -0500

    atualiza total sql itens

--- class/dao/financeiro/ordem/DaoFinOrdem.class.php
+++ class/dao/financeiro/ordem/DaoFinOrdem.class.php
@@ -54,13 +54,7 @@ class DaoFinOrdem extends FinOrdemTb {
             if (!empty($pdo)) {
                 $sql = "select pedido.id_pedido, pre.id_pre_ordem, itens.nr_item, mat.nm_material, mat.nm_grupo, mat.nm_sub_grupo,
                         unid.nm_unidade_medida, desp.ds_despesa_elemento, mat.tp_material, pre.qt_itens_pre, pre.vl_itens_pre,
-                        mat.nm_desc_material, itens.nr_lote, itens.fl_valor_variavel,
-                        case 
-                                when (mat.tp_material = 'C' or mat.tp_material = 'P') and itens.fl_valor_variavel = '0' 
-                            then pre.qt_itens_pre
-                            when mat.tp_material = 'S' or itens.fl_valor_variavel = '1'
-                            then pre.qt_itens_pre * pre.vl_itens_pre 
-                        end total,
+                        mat.nm_desc_material, itens.nr_lote, itens.fl_valor_variavel, (pre.qt_itens_pre * pre.vl_itens_pre ) as total,
                         case 
                                 when (mat.tp_material = 'C' or mat.tp_material = 'P') and itens.fl_valor_variavel = '0' 
                             then  coalesce(ordemItens.qt_itens_ordem,'0.0000') + coalesce(entregas.qt_itens_entrega,'0.0000')
