da098cac42a95b8dd2ab2d83872073661a75418f
Joellytton mendonça dos santos <joellytton.mendonca@ac.gov.br>
2018-11-08 11:33:45 -0500

    atualiza

--- pages/contabil/pagamento/edit_pagamento/index.js
+++ pages/contabil/pagamento/edit_pagamento/index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var url = "request.php";
 
     $("#nr_pagamento").mask("9999999999/9999");
-    
+
     //select2
     $('body').find('select').select2({
         width: '100%'
@@ -97,6 +97,11 @@ $(document).ready(function () {
             $("#selectDocumentoFiscal").html("");
             $("#selectDocumentoFiscal").append(response);
         }
+    });
+
+    $('body').on('click', '.ver-liquidacao', function (e) {
+        var id = $(this).val();
+        window.open("/pages/contabil/liquidacao/ver_liquidacao/index.php?&id=" + id);
     });
 
 
