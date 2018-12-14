48bddf72e72c7c70cb43197df88ee181be6fa3b8
elivelton.lima <eliveltonlima999@gmail.com>
2018-12-14 14:39:26 -0500

    Contratos

--- model/rh/pessoaFisica/index.load.php
+++ model/rh/pessoaFisica/index.load.php
@@ -1,9 +1,14 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/class/util/Session.class.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/class/rh/Contrato.class.php";
 
 $session = new Session();
 
 if(!$session->vPRh()){
     header("Location: /pages/index.php"); 
+} else {
+    $cpf = explode("=", $_GET['id'])[0];
+    $contrato = new Contrato();
+    $qtContratoPessoa = ($contrato->retornaQtContratoPessoa($cpf)+1).'° Contrato';
 }
 ?>
